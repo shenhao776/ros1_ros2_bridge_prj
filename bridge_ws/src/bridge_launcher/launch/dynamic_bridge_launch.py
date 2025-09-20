cebbf318680c1e59274590c23299b375a1027078
Shen Hao <shenhao776@gmail.com>
2025-09-20 20:06:06 +0900

    update gitignore

--- bridge_ws/src/bridge_launcher/launch/dynamic_bridge_launch.py
+++ bridge_ws/src/bridge_launcher/launch/dynamic_bridge_launch.py
@@ -3,7 +3,8 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     """
-    Generates the launch description for starting the ros1_bridge dynamic_bridge.
+    Generates the launch description for starting the ros1_bridge dynamic_bridge
+    with topic remapping.
     """
     return LaunchDescription([
         Node(
@@ -13,6 +14,14 @@ def generate_launch_description():
             output='screen',
             arguments=[
                 '--bridge-all-2to1-topics'
+                # '/camera/camera/color/image_raw@sensor_msgs/msg/Image@/camera/color/image_raw',
+
+                # 示例: 将 ROS 1 的 /scan (类型: sensor_msgs/LaserScan) 桥接到 ROS 2
+                # '/scan@sensor_msgs/LaserScan@{'
+            ],
+            remappings=[
+                # 修改输入名: ('ROS 2中使用的话题名', '桥接后在ROS 1中使用的话题名')
+                # ('/camera/camera/color/image_raw', '/camera/color/image_raw')
             ]
         )
     ])
